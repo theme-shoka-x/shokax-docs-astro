2b248c2ef9dbf8bf9bc3da1426ba6ebe41622a16
zkz098 <zkz20081204@126.com>
2026-02-14 22:45:06 +0800

    chore: 自定义css框架

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'ShokaX Astro Blog Theme',
+			customCss: [
+				'./src/styles/custom.css',
+			],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/theme-shoka-x/astro-blog-shokax' }],
 			sidebar: [
 				{
